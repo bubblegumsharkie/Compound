c79739bde5bb62d33fdfc6e9cdef5758f354b353
Alex <kozlov.alex.dev@gmail.com>
2023-05-11 16:43:30 +0300

    ViewBinding impl

--- app/src/main/java/com/countlesswrongs/compound/presentation/fragment/GameFinishedFragment.kt
+++ app/src/main/java/com/countlesswrongs/compound/presentation/fragment/GameFinishedFragment.kt
@@ -1,20 +1,30 @@
 package com.countlesswrongs.compound.presentation.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.countlesswrongs.compound.R
+import androidx.fragment.app.Fragment
+import com.countlesswrongs.compound.databinding.FragmentGameFinishedBinding
 
 
 class GameFinishedFragment : Fragment() {
 
+    private var _binding: FragmentGameFinishedBinding? = null
+    private val binding: FragmentGameFinishedBinding
+        get() = _binding ?: throw RuntimeException("FragmentGameFinishedBinding is NULL")
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_game_finished, container, false)
+    ): View {
+        _binding = FragmentGameFinishedBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
--- app/src/main/java/com/countlesswrongs/compound/presentation/fragment/PickLevelFragment.kt
+++ app/src/main/java/com/countlesswrongs/compound/presentation/fragment/PickLevelFragment.kt
@@ -5,16 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.countlesswrongs.compound.R
+import com.countlesswrongs.compound.databinding.FragmentPickLevelBinding
 
 
 class PickLevelFragment : Fragment() {
 
+    private var _binding: FragmentPickLevelBinding? = null
+    private val binding: FragmentPickLevelBinding
+        get() = _binding ?: throw RuntimeException("FragmentPickLevelBinding is NULL")
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_pick_level, container, false)
+    ): View {
+        _binding = FragmentPickLevelBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
