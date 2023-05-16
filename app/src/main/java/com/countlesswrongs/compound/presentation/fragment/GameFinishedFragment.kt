3d172f1359bfff379c6d5bbb58f68a9c13066dca
Alex <kozlov.alex.dev@gmail.com>
2023-05-16 10:12:00 +0300

    removing code with no use now

--- app/src/main/java/com/countlesswrongs/compound/presentation/fragment/GameFinishedFragment.kt
+++ app/src/main/java/com/countlesswrongs/compound/presentation/fragment/GameFinishedFragment.kt
@@ -4,20 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.countlesswrongs.compound.R
 import com.countlesswrongs.compound.databinding.FragmentGameFinishedBinding
 
 
 class GameFinishedFragment : Fragment() {
 
     private val args by navArgs<GameFinishedFragmentArgs>()
-    private val gameResult by lazy {
-        args.gameResult
-    }
 
     private var _binding: FragmentGameFinishedBinding? = null
     private val binding: FragmentGameFinishedBinding
@@ -34,39 +29,8 @@ class GameFinishedFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUpFragmentControls()
-        setEmoji()
-        showResults()
-    }
+        binding.gameResult = args.gameResult
 
-    private fun showResults() {
-        binding.gameResult = gameResult
-//        with(binding) {
-//            tvRequiredAnswers.text = String.format(
-//                resources.getString(R.string.tv_required_score),
-//                gameResult.gameSettings.minAmountOfRightAnswers.toString()
-//            )
-//            tvScoreAnswers.text = String.format(
-//                resources.getString(R.string.tv_score_answers),
-//                gameResult.amountOfRightAnswers.toString()
-//            )
-//            tvRequiredPercentage.text = String.format(
-//                resources.getString(R.string.tv_required_percentage),
-//                gameResult.gameSettings.minPercentOfRightAnswers.toString()
-//            )
-//            tvScorePercentage.text = String.format(
-//                resources.getString(R.string.tv_score_percentage),
-//                gameResult.percentageOfRightAnswers.toString()
-//            )
-//        }
-    }
-
-    private fun setEmoji() {
-        val drawable = if (gameResult.didWin) {
-            ContextCompat.getDrawable(requireContext(), R.drawable.ic_happy)
-        } else {
-            ContextCompat.getDrawable(requireContext(), R.drawable.ic_sad)
-        }
-        binding.ivEmojiResult.setImageDrawable(drawable)
     }
 
     private fun setUpFragmentControls() {
